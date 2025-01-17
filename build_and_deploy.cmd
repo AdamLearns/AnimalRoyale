6ab19a76f2c98b9fb46e13719c6251dd22e521c2
Adam Damiano <adam@adamlearns.live>
2025-01-16 20:56:08 -0800

    Add --offline to Maven command
    Kevin had suggested this so that I don't get stuck fetching dependencies
    for many minutes.

--- build_and_deploy.cmd
+++ build_and_deploy.cmd
@@ -3,7 +3,8 @@
 :: This is a simple script to build and copy the JAR file to the right target.
 :: This was really only intended to be run on my own machine.
 
-call mvn clean install
+:: Use "--offline" so that it doesn't fetch a million dependencies for no reason
+call mvn clean install --offline
 copy /y .\target\animalroyale-1.0-SNAPSHOT.jar ..\PaperSpigot\plugins\
 
 echo "Don't forget to do '/rl confirm' from Minecraft!"
--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -3,7 +3,8 @@
 # This is a simple script to build and copy the JAR file to the right target.
 # This was really only intended to be run on my own machine.
 
-mvn clean install
+# Use "--offline" so that it doesn't fetch a million dependencies for no reason
+mvn clean install --offline
 /bin/cp -f ./target/animalroyale-1.0-SNAPSHOT.jar /Users/adam/Downloads/paper/plugins
 
 echo "Don't forget to do '/rl confirm' from Minecraft!"
